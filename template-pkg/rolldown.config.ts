7d4dce14479ce653cee98927a82140be81258969
yassine-zhang <hi@itcox.cn>
2025-07-02 18:45:26 +0800

    feat(pkgs): 新增包模版，更新文档说明

---
+++ template-pkg/rolldown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "rolldown";
+
+export default defineConfig({
+  input: "./index.ts",
+  output: {
+    format: "esm",
+    dir: "dist",
+    minify: true,
+  },
+  external: [],
+});
