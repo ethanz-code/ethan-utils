bd5d2371f09003bb326930080841141041e4347d
yassine-zhang <hi@itcox.cn>
2025-07-05 22:24:47 +0800

    feat(index): 更新欢迎信息；更新多个包的构建脚本，添加 module 类型声明

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import chalk from "chalk";
 import cowsay from "@ethan-utils/cowsay";
 
 console.log(
-  chalk.bgYellowBright.white("欢迎使用 Ethan Zhang 的 Vue, NodeJS 开发工具包"),
+  chalk.bgYellowBright.white(
+    "欢迎使用 Ethan Zhang 的 Vue, React, NodeJS 开发工具包",
+  ),
 );
 console.log(chalk.bgBlueBright.white.bold("联系方式：hi@itcox.cn"));
 
